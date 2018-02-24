336c4ac10937b91a97486d9e08b7e811d43867c2
Jorge Rambla Gonzalez <718911@unizar.es>
2018-02-24 16:35:19 +0100

    Arreglo fallo de usuarioDAO en juegoDAO Signed-off-by: Jorge Rambla Gonzalez <718911@unizar.es>

--- src/modelo/datos/DAO/UsuarioDAO.java
+++ src/modelo/datos/DAO/UsuarioDAO.java
@@ -3,10 +3,218 @@
  */
 package modelo.datos.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * @author Jorge Rambla
  *
  */
 public class UsuarioDAO {
+  public String getSeudonimo(String email, Connection conexion) {
+    String retVal = "Error";
+    try {
+      String query = "SELECT seudonimo FROM usuario WHERE email = ?";
+      PreparedStatement ps = conexion.prepareStatement(query);
 
+      ps.setString(1, email);
+
+      ResultSet rs = ps.executeQuery();
+
+      if (!rs.first()) {
+        throw new SQLException(
+            "Error: No se ha encontrado ningun usuario con el email de " + email);
+      } else {
+        retVal = rs.getString(1);
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace(System.err);
+    }
+    return retVal;
+  }
+
+  public String getNombre(String seudonimo, Connection conexion) {
+    String retVal = "Error";
+    try {
+      String query = "SELECT nombre FROM usuario WHERE seudonimo = ?";
+      PreparedStatement ps = conexion.prepareStatement(query);
+
+      ps.setString(1, seudonimo);
+
+      ResultSet rs = ps.executeQuery();
+
+      if (!rs.first()) {
+        throw new SQLException(
+            "Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
+      } else {
+        retVal = rs.getString(1);
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace(System.err);
+    }
+    return retVal;
+  }
+
+  public String getEmail(String seudonimo, Connection conexion) {
+    String retVal = "Error";
+    try {
+      String query = "SELECT email FROM usuario WHERE seudonimo = ?";
+      PreparedStatement ps = conexion.prepareStatement(query);
+
+      ps.setString(1, seudonimo);
+
+      ResultSet rs = ps.executeQuery();
+
+      if (!rs.first()) {
+        throw new SQLException(
+            "Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
+      } else {
+        retVal = rs.getString(1);
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace(System.err);
+    }
+    return retVal;
+  }
+
+  public String getPassword(String seudonimo, Connection conexion) {
+    String retVal = "Error";
+    try {
+      String query = "SELECT password FROM usuario WHERE seudonimo = ?";
+      PreparedStatement ps = conexion.prepareStatement(query);
+
+      ps.setString(1, seudonimo);
+
+      ResultSet rs = ps.executeQuery();
+
+      if (!rs.first()) {
+        throw new SQLException(
+            "Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
+      } else {
+        retVal = rs.getString(1);
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace(System.err);
+    }
+    return retVal;
+  }
+
+  public String getImagen(String seudonimo, Connection conexion) {
+    String retVal = "Error";
+    try {
+      String query = "SELECT imagen FROM usuario WHERE seudonimo = ?";
+      PreparedStatement ps = conexion.prepareStatement(query);
+
+      ps.setString(1, seudonimo);
+
+      ResultSet rs = ps.executeQuery();
+
+      if (!rs.first()) {
+        throw new SQLException(
+            "Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
+      } else {
+        retVal = rs.getString(1);
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace(System.err);
+    }
+    return retVal;
+  }
+
+  public int getNivel(String seudonimo, Connection conexion) {
+    int retVal = -1;
+    try {
+      String query = "SELECT nivel FROM usuario WHERE seudonimo = ?";
+      PreparedStatement ps = conexion.prepareStatement(query);
+
+      ps.setString(1, seudonimo);
+
+      ResultSet rs = ps.executeQuery();
+
+      if (!rs.first()) {
+        throw new SQLException(
+            "Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
+      } else {
+        retVal = rs.getInt(1);
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace(System.err);
+    }
+    return retVal;
+  }
+
+  public int getExperiencia(String seudonimo, Connection conexion) {
+    int retVal = -1;
+    try {
+      String query = "SELECT experiencia FROM usuario WHERE seudonimo = ?";
+      PreparedStatement ps = conexion.prepareStatement(query);
+
+      ps.setString(1, seudonimo);
+
+      ResultSet rs = ps.executeQuery();
+      if (!rs.first()) {
+        throw new SQLException(
+            "Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
+      } else {
+        retVal = rs.getInt(1);
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace(System.err);
+    }
+    return retVal;
+  }
+
+  /**
+   * @param seudonimo
+   * @param email
+   * @param password
+   * @param conexion
+   * @return true si encuentra un usuario con igual nombre y contraseña, o email y contraseña
+   */
+  public boolean existeUsuario(String seudonimo, String email, String password,
+      Connection conexion) {
+    boolean retVal = false;
+
+    try {
+      if (seudonimo != null) {
+        String query = "SELECT * FROM usuario WHERE seudonimo = ? AND password = ?";
+
+        PreparedStatement ps = conexion.prepareStatement(query);
+
+        ps.setString(1, seudonimo);
+        ps.setString(2, password);
+
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.first()) {
+          return true;
+        }
+      } else if (email != null) {
+        String query = "SELECT * FROM usuario WHERE email = ? AND password = ?";
+
+        PreparedStatement ps = conexion.prepareStatement(query);
+
+        ps.setString(1, email);
+        ps.setString(2, password);
+
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.first()) {
+          return true;
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace(System.err);;
+    }
+    return retVal;
+  }
 }
