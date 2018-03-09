5a35fe7ab74bf5d2bec0592ce9c2885c1c32e1a2
Jorge Rambla Gonzalez <718911@unizar.es>
2018-03-09 11:42:13 +0100

    +Metodo fachada & DAO insertar usuario
    Signed-off-by: Jorge Rambla Gonzalez <718911@unizar.es>

--- src/modelo/datos/DAO/UsuarioDAO.java
+++ src/modelo/datos/DAO/UsuarioDAO.java
@@ -15,263 +15,276 @@ import modelo.datos.VO.UsuarioVO;
  *
  */
 public class UsuarioDAO {
-  
-  /**
-   * @param seudonimo
-   * @param conexion
-   * @return UsuarioVO vacio si no existe ningun usuario con el seudonimo especificado <br>
-   *         UsuarioVO con el seudonimo especificado
-   */
-  public UsuarioVO getUsuario(String seudonimo, Connection conexion) {
-    UsuarioVO retVal = new UsuarioVO();
-    try {
-      String query = "SELECT * FROM usuario WHERE seudonimo = ?";
 
-      PreparedStatement ps = conexion.prepareStatement(query);
+	/**
+	 * @param seudonimo
+	 * @param conexion
+	 * @return UsuarioVO vacio si no existe ningun usuario con el seudonimo
+	 *         especificado <br>
+	 *         UsuarioVO con el seudonimo especificado
+	 */
+	public UsuarioVO getUsuario(String seudonimo, Connection conexion) {
+		UsuarioVO retVal = new UsuarioVO();
+		try {
+			String query = "SELECT * FROM usuario WHERE seudonimo = ?";
 
-      ps.setString(1, seudonimo);
+			PreparedStatement ps = conexion.prepareStatement(query);
 
-      ResultSet rs = ps.executeQuery();
+			ps.setString(1, seudonimo);
 
-      if (!rs.first()) {
-        throw new SQLException(
-            "Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
-      } else {
-        retVal = new UsuarioVO(rs.getString(1), rs.getString(2), rs.getString(3),
-            rs.getString(4), rs.getString(5), rs.getInt(6), rs.getInt(7));
-      }
-    } catch (Exception e) {
-      e.printStackTrace(System.err);
-    }
+			ResultSet rs = ps.executeQuery();
 
-    return retVal;
-  }
+			if (!rs.first()) {
+				throw new SQLException("Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
+			} else {
+				retVal = new UsuarioVO(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),
+						rs.getString(5), rs.getInt(6), rs.getInt(7));
+			}
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
 
-  public ArrayList<UsuarioVO> getAllUsuario(Connection conexion) {
-    ArrayList<UsuarioVO> retVal = new ArrayList<UsuarioVO>();
-    try {
-      String query = "SELECT * from usuario";
+		return retVal;
+	}
 
-      PreparedStatement ps = conexion.prepareStatement(query);
+	public ArrayList<UsuarioVO> getAllUsuario(Connection conexion) {
+		ArrayList<UsuarioVO> retVal = new ArrayList<UsuarioVO>();
+		try {
+			String query = "SELECT * from usuario";
 
-      ResultSet rs = ps.executeQuery();
+			PreparedStatement ps = conexion.prepareStatement(query);
 
-      if (!rs.first()) {
-        throw new SQLException("Error: No hay ningun usuario en la tabla usuario");
-      } else {
-         do {
-          retVal.add(new UsuarioVO(rs.getString(1), rs.getString(2), rs.getString(3),
-              rs.getString(4), rs.getString(5), rs.getInt(6), rs.getInt(7)));
-        }while (rs.next());
-      }
-    } catch (Exception e) {
-      e.printStackTrace(System.err);
-    }
-    return retVal;
-  }
+			ResultSet rs = ps.executeQuery();
 
-  public String getSeudonimo(String email, Connection conexion) {
-    String retVal = "Error";
-    try {
-      String query = "SELECT seudonimo FROM usuario WHERE email = ?";
-      PreparedStatement ps = conexion.prepareStatement(query);
+			if (!rs.first()) {
+				throw new SQLException("Error: No hay ningun usuario en la tabla usuario");
+			} else {
+				do {
+					retVal.add(new UsuarioVO(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),
+							rs.getString(5), rs.getInt(6), rs.getInt(7)));
+				} while (rs.next());
+			}
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+		return retVal;
+	}
 
-      ps.setString(1, email);
+	public String getSeudonimo(String email, Connection conexion) {
+		String retVal = "Error";
+		try {
+			String query = "SELECT seudonimo FROM usuario WHERE email = ?";
+			PreparedStatement ps = conexion.prepareStatement(query);
 
-      ResultSet rs = ps.executeQuery();
+			ps.setString(1, email);
 
-      if (!rs.first()) {
-        throw new SQLException(
-            "Error: No se ha encontrado ningun usuario con el email de " + email);
-      } else {
-        retVal = rs.getString(1);
-      }
+			ResultSet rs = ps.executeQuery();
 
-    } catch (Exception e) {
-      e.printStackTrace(System.err);
-    }
-    return retVal;
-  }
+			if (!rs.first()) {
+				throw new SQLException("Error: No se ha encontrado ningun usuario con el email de " + email);
+			} else {
+				retVal = rs.getString(1);
+			}
 
-  public String getNombre(String seudonimo, Connection conexion) {
-    String retVal = "Error";
-    try {
-      String query = "SELECT nombre FROM usuario WHERE seudonimo = ?";
-      PreparedStatement ps = conexion.prepareStatement(query);
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+		return retVal;
+	}
 
-      ps.setString(1, seudonimo);
+	public String getNombre(String seudonimo, Connection conexion) {
+		String retVal = "Error";
+		try {
+			String query = "SELECT nombre FROM usuario WHERE seudonimo = ?";
+			PreparedStatement ps = conexion.prepareStatement(query);
 
-      ResultSet rs = ps.executeQuery();
+			ps.setString(1, seudonimo);
 
-      if (!rs.first()) {
-        throw new SQLException(
-            "Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
-      } else {
-        retVal = rs.getString(1);
-      }
+			ResultSet rs = ps.executeQuery();
 
-    } catch (Exception e) {
-      e.printStackTrace(System.err);
-    }
-    return retVal;
-  }
+			if (!rs.first()) {
+				throw new SQLException("Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
+			} else {
+				retVal = rs.getString(1);
+			}
 
-  public String getEmail(String seudonimo, Connection conexion) {
-    String retVal = "Error";
-    try {
-      String query = "SELECT email FROM usuario WHERE seudonimo = ?";
-      PreparedStatement ps = conexion.prepareStatement(query);
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+		return retVal;
+	}
 
-      ps.setString(1, seudonimo);
+	public String getEmail(String seudonimo, Connection conexion) {
+		String retVal = "Error";
+		try {
+			String query = "SELECT email FROM usuario WHERE seudonimo = ?";
+			PreparedStatement ps = conexion.prepareStatement(query);
 
-      ResultSet rs = ps.executeQuery();
+			ps.setString(1, seudonimo);
 
-      if (!rs.first()) {
-        throw new SQLException(
-            "Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
-      } else {
-        retVal = rs.getString(1);
-      }
+			ResultSet rs = ps.executeQuery();
 
-    } catch (Exception e) {
-      e.printStackTrace(System.err);
-    }
-    return retVal;
-  }
+			if (!rs.first()) {
+				throw new SQLException("Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
+			} else {
+				retVal = rs.getString(1);
+			}
 
-  public String getPassword(String seudonimo, Connection conexion) {
-    String retVal = "Error";
-    try {
-      String query = "SELECT password FROM usuario WHERE seudonimo = ?";
-      PreparedStatement ps = conexion.prepareStatement(query);
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+		return retVal;
+	}
 
-      ps.setString(1, seudonimo);
+	public String getPassword(String seudonimo, Connection conexion) {
+		String retVal = "Error";
+		try {
+			String query = "SELECT password FROM usuario WHERE seudonimo = ?";
+			PreparedStatement ps = conexion.prepareStatement(query);
 
-      ResultSet rs = ps.executeQuery();
+			ps.setString(1, seudonimo);
 
-      if (!rs.first()) {
-        throw new SQLException(
-            "Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
-      } else {
-        retVal = rs.getString(1);
-      }
+			ResultSet rs = ps.executeQuery();
 
-    } catch (Exception e) {
-      e.printStackTrace(System.err);
-    }
-    return retVal;
-  }
+			if (!rs.first()) {
+				throw new SQLException("Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
+			} else {
+				retVal = rs.getString(1);
+			}
 
-  public String getImagen(String seudonimo, Connection conexion) {
-    String retVal = "Error";
-    try {
-      String query = "SELECT imagen FROM usuario WHERE seudonimo = ?";
-      PreparedStatement ps = conexion.prepareStatement(query);
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+		return retVal;
+	}
 
-      ps.setString(1, seudonimo);
+	public String getImagen(String seudonimo, Connection conexion) {
+		String retVal = "Error";
+		try {
+			String query = "SELECT imagen FROM usuario WHERE seudonimo = ?";
+			PreparedStatement ps = conexion.prepareStatement(query);
 
-      ResultSet rs = ps.executeQuery();
+			ps.setString(1, seudonimo);
 
-      if (!rs.first()) {
-        throw new SQLException(
-            "Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
-      } else {
-        retVal = rs.getString(1);
-      }
+			ResultSet rs = ps.executeQuery();
 
-    } catch (Exception e) {
-      e.printStackTrace(System.err);
-    }
-    return retVal;
-  }
+			if (!rs.first()) {
+				throw new SQLException("Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
+			} else {
+				retVal = rs.getString(1);
+			}
 
-  public int getNivel(String seudonimo, Connection conexion) {
-    int retVal = -1;
-    try {
-      String query = "SELECT nivel FROM usuario WHERE seudonimo = ?";
-      PreparedStatement ps = conexion.prepareStatement(query);
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+		return retVal;
+	}
 
-      ps.setString(1, seudonimo);
+	public int getNivel(String seudonimo, Connection conexion) {
+		int retVal = -1;
+		try {
+			String query = "SELECT nivel FROM usuario WHERE seudonimo = ?";
+			PreparedStatement ps = conexion.prepareStatement(query);
 
-      ResultSet rs = ps.executeQuery();
+			ps.setString(1, seudonimo);
 
-      if (!rs.first()) {
-        throw new SQLException(
-            "Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
-      } else {
-        retVal = rs.getInt(1);
-      }
+			ResultSet rs = ps.executeQuery();
 
-    } catch (Exception e) {
-      e.printStackTrace(System.err);
-    }
-    return retVal;
-  }
+			if (!rs.first()) {
+				throw new SQLException("Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
+			} else {
+				retVal = rs.getInt(1);
+			}
 
-  public int getExperiencia(String seudonimo, Connection conexion) {
-    int retVal = -1;
-    try {
-      String query = "SELECT experiencia FROM usuario WHERE seudonimo = ?";
-      PreparedStatement ps = conexion.prepareStatement(query);
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+		return retVal;
+	}
 
-      ps.setString(1, seudonimo);
+	public int getExperiencia(String seudonimo, Connection conexion) {
+		int retVal = -1;
+		try {
+			String query = "SELECT experiencia FROM usuario WHERE seudonimo = ?";
+			PreparedStatement ps = conexion.prepareStatement(query);
 
-      ResultSet rs = ps.executeQuery();
-      if (!rs.first()) {
-        throw new SQLException(
-            "Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
-      } else {
-        retVal = rs.getInt(1);
-      }
+			ps.setString(1, seudonimo);
 
-    } catch (Exception e) {
-      e.printStackTrace(System.err);
-    }
-    return retVal;
-  }
+			ResultSet rs = ps.executeQuery();
+			if (!rs.first()) {
+				throw new SQLException("Error: No se ha encontrado ningun usuario con el seudonimo de " + seudonimo);
+			} else {
+				retVal = rs.getInt(1);
+			}
 
-  /**
-   * @param seudonimo
-   * @param email
-   * @param password
-   * @param conexion
-   * @return true si encuentra un usuario con igual nombre y contrase�a, o email y contrase�a
-   */
-  public boolean existeUsuario(String seudonimo, String email, String password,
-      Connection conexion) {
-    boolean retVal = false;
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+		return retVal;
+	}
 
-    try {
-      if (seudonimo != null) {
-        String query = "SELECT * FROM usuario WHERE seudonimo = ? AND password = ?";
+	/**
+	 * @param seudonimo
+	 * @param email
+	 * @param password
+	 * @param conexion
+	 * @return true si encuentra un usuario con igual nombre y contrase�a, o email y
+	 *         contrase�a
+	 */
+	public boolean existeUsuario(String seudonimo, String email, String password, Connection conexion) {
+		boolean retVal = false;
 
-        PreparedStatement ps = conexion.prepareStatement(query);
+		try {
+			if (seudonimo != null) {
+				String query = "SELECT * FROM usuario WHERE seudonimo = ? AND password = ?";
 
-        ps.setString(1, seudonimo);
-        ps.setString(2, password);
+				PreparedStatement ps = conexion.prepareStatement(query);
 
-        ResultSet rs = ps.executeQuery();
+				ps.setString(1, seudonimo);
+				ps.setString(2, password);
 
-        if (rs.first()) {
-          return true;
-        }
-      } else if (email != null) {
-        String query = "SELECT * FROM usuario WHERE email = ? AND password = ?";
+				ResultSet rs = ps.executeQuery();
 
-        PreparedStatement ps = conexion.prepareStatement(query);
+				if (rs.first()) {
+					return true;
+				}
+			} else if (email != null) {
+				String query = "SELECT * FROM usuario WHERE email = ? AND password = ?";
 
-        ps.setString(1, email);
-        ps.setString(2, password);
+				PreparedStatement ps = conexion.prepareStatement(query);
 
-        ResultSet rs = ps.executeQuery();
+				ps.setString(1, email);
+				ps.setString(2, password);
 
-        if (rs.first()) {
-          return true;
-        }
-      }
-    } catch (Exception e) {
-      e.printStackTrace(System.err);;
-    }
-    return retVal;
-  }
+				ResultSet rs = ps.executeQuery();
+
+				if (rs.first()) {
+					return true;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			;
+		}
+		return retVal;
+	}
+
+	public void insertarUsuario(UsuarioVO usuario, Connection conexion) {
+		try {
+			String query = "INSERT INTO usuario (seudonimo, nombre, email, password) VALUES (?,?,?,?)";
+			
+			PreparedStatement ps = conexion.prepareStatement(query);
+			
+			ps.setString(1, usuario.getSeudonimo());
+			ps.setString(2, usuario.getNombre());
+			ps.setString(3, usuario.getEmail());
+			ps.setString(4, usuario.getPassword());
+			
+			if(ps.executeUpdate() != 1) {
+				throw new SQLException("Ha habido problemas a la hora de insertar el usuario");
+			}
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+	}
 }
--- src/modelo/datos/WebFacade.java
+++ src/modelo/datos/WebFacade.java
@@ -16,50 +16,20 @@ import modelo.datos.VO.UsuarioVO;
  *
  */
 public class WebFacade {
-  public static void test(String email, int id_juego) throws SQLException{
-    Connection conexion = null;
-    
-    try {
-      conexion = GestorDeConexionesBD.getConnection();
-      UsuarioDAO aux = new UsuarioDAO();
-      String seudonimo = aux.getSeudonimo(email, conexion);
-      System.out.println(seudonimo);
-      System.out.println(aux.getEmail(seudonimo, conexion));
-      System.out.println(aux.getExperiencia(seudonimo, conexion));
-      System.out.println(aux.getImagen(seudonimo, conexion));
-      System.out.println(aux.getNivel(seudonimo, conexion));
-      System.out.println(aux.getNombre(seudonimo, conexion));
-      System.out.println(aux.getPassword(seudonimo, conexion));
-      
-      System.out.println();
-      
-      System.out.println(aux.getUsuario(seudonimo, conexion));
-      
-      System.out.println("Todos los usuarios:");
-      
-      for (UsuarioVO u : aux.getAllUsuario(conexion)){
-        System.out.println(u);
-      }
-      
-      System.out.println("Pruebas de juegos \n\n");
-      
-      JuegoDAO aux2 = new JuegoDAO();
-      
-      System.out.println(aux2.getJuego(id_juego, conexion));
-      System.out.println("Todos los usuarios:");
-
-      for (JuegoVO juego : aux2.getAllJuego(conexion)){
-        System.out.println(juego);
-      }
-    } catch (Exception e){
-      e.printStackTrace(System.err);
-      System.out.println("Excepcion en el metodo fachada y no lanza hacia arriba");
-    } finally {
-      conexion.close();
-    }
+	
+  public void insertarUsuario(UsuarioVO usuario) throws SQLException {
+	  Connection conexion = null;
+	  try {
+		  conexion = GestorDeConexionesBD.getConnection();
+		  new UsuarioDAO().insertarUsuario(usuario, conexion);
+	  } catch (Exception e) {
+		  e.printStackTrace(System.err);
+	  } finally {
+		  conexion.close();
+	  }
   }
   
   public static void main(String[] args) throws SQLException {
-    test("yorch044zgz@gmail.com", 1);
+    
   }
 }
